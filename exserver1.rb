00dd52df4902ca9c5fb60314cb53d7c2409a1702
Sharell Bryant <sharell.bryant@gmail.com>
2014-09-10 13:29:35 -0400

    Responds to http GET request

--- exserver1.rb
+++ exserver1.rb
@@ -8,8 +8,21 @@ loop do
 	Thread.start(server.accept) do |client| #usable for serving multiple clients
 
 		request = client.read_nonblock(256) #read the request from the client and store it in the request variable
+		header, body = request.split("\r\n\r\n", 2)#split request into header and body (two line breaks and then max limit of 2)
+		file_path = header.split[1][1..-1] #get path from request header
 
-		if #file exists
+
+		if File.exists? file_path #if file exists
+			body = File.read(file_path)
+			client.puts "GET #{filepath} HTTP/1.0 200 OK\r\n"
+			client.puts "Content size: #{File.size(file_path)}\r\n\r\n}"
+			if header.start_with? 'GET'
+				client.puts body
+			else
+				client.puts "Unknown request error"
+			end
 		else #if file does not exist
+			client.puts "HTTP/1.0 404 Not Found\r\n\r\n"
 		end
+		client.close
 end
